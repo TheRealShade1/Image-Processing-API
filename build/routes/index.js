3b6ee5e08d0b3b42b0bb54bbd07fc602bdbc8c1f
Ahmed Khaled Abdelaziz <95549767+ahmedkhaled9696@users.noreply.github.com>
2022-06-25 20:26:03 +0200

    Update index.js

--- build/routes/index.js
+++ build/routes/index.js
@@ -4,34 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const sharp_resize_1 = __importDefault(require("../utilities/sharp-resize"));
-const routes = express_1.default.Router();
-/* GET home page. */
-routes.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Image Sizer',
-        h1Text: 'Web Image Size Generator',
-        pText: 'Upload and resize an image to get the most common sizes used for the web'
-    });
+const index_1 = __importDefault(require("./routes/index"));
+const app = (0, express_1.default)();
+const port = 3000;
+// Set the routes
+app.use('/api', index_1.default);
+// Start the server
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}/`);
 });
-// route to display resized images
-routes.post('/processed-images', upload.single('imageupload'), (req, res) => {
-    let resizedImgs = [];
-    try {
-        resizedImgs = (0, sharp_resize_1.default)(imgUrl);
-        if (resizedImgs.length === 7) {
-            setTimeout(() => {
-                res.render('processed-images', {
-                    title: 'Image Sizer',
-                    h1Text: 'Web Image Size Generator',
-                    pText: 'Upload and resize an image to get the most common sizes used for the web',
-                    imgUrls: resizedImgs
-                });
-            }, 600);
-        }
-    }
-    catch (err) {
-        console.error('Image resizing error:', err);
-    }
-});
-exports.default = routes;
+exports.default = app;
