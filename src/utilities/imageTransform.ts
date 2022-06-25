9554c8a2edeb32d41d604fc936fc920b8c769e3e
Ahmed Khaled Abdelaziz <95549767+ahmedkhaled9696@users.noreply.github.com>
2022-06-25 20:31:24 +0200

    Create imageTransform.ts

---
+++ src/utilities/imageTransform.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import sharp from 'sharp';
+import { promises as fsPromises } from 'fs';
+import fs from 'fs';
+
+// Resizes the image to the width and height set in the parameters
+const imageTransform = async (
+  filename: string,
+  width: number,
+  height: number
+): Promise<string> => {
+  const imageInput: string =
+    path.join(__dirname, '../', '../', 'assets/', 'originals/', filename) +
+    '.jpg';
+  const imageOutputFolder: string = path.join(
+    __dirname,
+    '../',
+    '../',
+    'assets/',
+    'thumbnails/'
+  );
+  const imageOutput: string =
+    path.join(__dirname, '../', '../', 'assets/', 'thumbnails/', filename) +
+    `-${width}-${height}.jpg`;
+
+  // if the image output folder doesn't exists, create it
+  if (!fs.existsSync(imageOutputFolder)) {
+    await fsPromises.mkdir(imageOutputFolder);
+  }
+
+  try {
+    // await for sharp to process the image, if it succeeds, returns the imageOutput
+    await sharp(imageInput).resize(width, height).toFile(imageOutput);
+    return imageOutput;
+  } catch (error) {
+    // if not, returns the error
+    return 'Image could not be processed.';
+  }
+};
+
+export default imageTransform;
