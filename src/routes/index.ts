535fc3e1a1eb2c0dd4ff59bf005b542af0f3569f
Ahmed Khaled Abdelaziz <95549767+ahmedkhaled9696@users.noreply.github.com>
2022-06-25 20:29:48 +0200

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,40 +1,56 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
-import imgResize from '../utilities/sharp-resize';
+import imageTransform from '../utilities/imageTransform';
+import { checkParams, IQuery } from '../utilities/checkParams';
 
 const routes = express.Router();
 
-/* GET home page. */
-routes.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Image Sizer',
-    h1Text: 'Web Image Size Generator',
-    pText:
-      'Upload and resize an image to get the most common sizes used for the web'
-  });
-});
+routes.get(
+  '/images',
+  async (req: express.Request, res: express.Response): Promise<void> => {
+    // check if all 3 mandatory parameters are set and width and height are numbers
+    if (checkParams((req.query as unknown) as IQuery)) {
+      // set each parameter to a variable
+      const filename = req.query.filename as string;
+      const width = Number(req.query.width);
+      const height = Number(req.query.height);
 
-// route to display resized images
-routes.post('/processed-images', upload.single('imageupload'), (req, res) => {
-  let resizedImgs: string[] | null = [];
-
-  try {
-    resizedImgs = imgResize(imgUrl);
-
-    if (resizedImgs.length === 7) {
-      setTimeout(() => {
-        res.render('processed-images', {
-          title: 'Image Sizer',
-          h1Text: 'Web Image Size Generator',
-          pText:
-            'Upload and resize an image to get the most common sizes used for the web',
-          imgUrls: resizedImgs
-        });
-      }, 600);
+      const imageThumbnail: string =
+        path.join(__dirname, '../', '../', 'assets/', 'thumbnails/', filename) +
+        `-${width}-${height}.jpg`;
+      // check if the requested image with the requested size already exists
+      if (fs.existsSync(imageThumbnail)) {
+        // if it does, send the existing image as response
+        res.sendFile(imageThumbnail);
+      } else {
+        // if it doesn't, resize the image and save the response to a variable
+        const imgProcessed = await imageTransform(
+          filename as string,
+          width,
+          height
+        );
+        // if the response doesn't include the word "Error", then it has been properly processed, send the processed image as response
+        if (!String(imgProcessed).includes('Error')) {
+          res.sendFile(imgProcessed);
+        } else {
+          // if the response includes the word "Error", send a proper error message
+          res
+            .status(404)
+            .send(
+              'bad request'
+            );
+        }
+      }
+    } else {
+      // if one of the parameters is missing in the request URL or if width or height are not numbers, then send a proper error message
+      res
+        .status(200)
+        .send(
+          'Please set a filename, width and height as parameters in the url (all 3 are mandatory and width and height must be numbers).'
+        );
     }
-  } catch (err) {
-    console.error('Image resizing error:', err);
   }
-});
+);
+
 export default routes;
