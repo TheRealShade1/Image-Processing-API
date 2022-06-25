19fb8a43a24ae3240ec695b7f7df5e7cd97e065f
Ahmed Khaled Abdelaziz <95549767+ahmedkhaled9696@users.noreply.github.com>
2022-06-25 20:26:28 +0200

    Update reporter.js

--- build/tests/helpers/reporter.js
+++ build/tests/helpers/reporter.js
@@ -3,14 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jasmine_spec_reporter_1 = require("jasmine-spec-reporter");
 class CustomProcessor extends jasmine_spec_reporter_1.DisplayProcessor {
     displayJasmineStarted(info, log) {
-        return `TypeScript ${log}`;
+        return `${log}`;
     }
 }
 jasmine.getEnv().clearReporters();
-jasmine.getEnv().addReporter(
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-new jasmine_spec_reporter_1.SpecReporter({
+jasmine.getEnv().addReporter(new jasmine_spec_reporter_1.SpecReporter({
     spec: {
         displayStacktrace: jasmine_spec_reporter_1.StacktraceOption.NONE,
     },
